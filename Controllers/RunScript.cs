c762541b29a4b90767de4600263aa0b82367c83f
Abdelrhman-Khamis <109832577+Abdelrhman-Khamis@users.noreply.github.com>
2022-11-11 21:32:59 +0200

    Add project files.

---
+++ Controllers/RunScript.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Smo;
+using Microsoft.EntityFrameworkCore;
+
+namespace SqlScript.Controllers
+{
+    public class RunScript : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ConnectionString.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SqlScript.Models
+{
+    [Table("ConnectionString", Schema = "dbo")]
+    public class ConnectionString
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "Connection String ID")]
+        public int ConnectionStringID { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(100)")]
+        [MaxLength(100)]
+        [Display(Name = "Connection String Name")]
+        public string ConnectionStringName { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(100)")]
+        [MaxLength(100)]
+        [Display(Name = "Connection String Data Source")]
+        public string ConnectionStringDataSource { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(100)")]
+        [MaxLength(100)]
+        [Display(Name = "Connection String UserID")]
+        public string ConnectionStringUserID { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(100)")]
+        [MaxLength(100)]
+        [Display(Name = "Connection String Password")]
+        public string ConnectionStringPassword { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(100)")]
+        [MaxLength(100)]
+        [Display(Name = "Connection String Initial Catalog")]
+        public string ConnectionStringInitialCatalog { get; set; }
+
+        [Required]
+        [ForeignKey("Connections")]
+        public int ConnectionsId { get; set; }
+
+        [Display(Name = "Connections")]
+        [NotMapped]
+        public string ConnectionsName { get; set; }
+
+        public virtual Connections Connections { get; set; }
+
+
+    }
+}
